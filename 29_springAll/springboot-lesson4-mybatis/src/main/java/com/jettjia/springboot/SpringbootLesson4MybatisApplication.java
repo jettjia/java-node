682b88990681ef9fde1bff788f05ab2956f3a31f
jidi <1608008898@qq.com>
2018-10-04 20:45:10 +0800

    add springboot mybatis demo

--- 29_springAll/springboot-lesson4-mybatis/src/main/java/com/jettjia/springboot/SpringbootLesson4MybatisApplication.java
+++ 29_springAll/springboot-lesson4-mybatis/src/main/java/com/jettjia/springboot/SpringbootLesson4MybatisApplication.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 @MapperScan(value = "com.jettjia.springboot.mapper")
+@SpringBootApplication
 public class SpringbootLesson4MybatisApplication {
 
     public static void main(String[] args) {
--- 29_springAll/springboot-lesson4-mybatis/src/main/java/com/jettjia/springboot/mapper/EmployeeMapper.java
+++ 29_springAll/springboot-lesson4-mybatis/src/main/java/com/jettjia/springboot/mapper/EmployeeMapper.java
@@ -1,10 +1,10 @@
 package com.jettjia.springboot.mapper;
 
 import com.jettjia.springboot.bean.Employee;
-import org.apache.ibatis.annotations.Mapper;
 
 //@Mapper或者@MapperScan将接口扫描装配到容器中
 public interface EmployeeMapper {
+
     public Employee getEmpById(Integer id);
 
     public void insertEmp(Employee employee);
