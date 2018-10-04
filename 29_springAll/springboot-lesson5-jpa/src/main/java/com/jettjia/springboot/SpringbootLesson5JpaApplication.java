af4ab6aa0692d3e1d73b3c41b8a7ed55a6b37613
jidi <1608008898@qq.com>
2018-10-04 21:01:27 +0800

    add springboot springdata jpa demo

---
+++ 29_springAll/springboot-lesson5-jpa/src/main/java/com/jettjia/springboot/SpringbootLesson5JpaApplication.java
@@ -0,0 +1,12 @@
+package com.jettjia.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLesson5JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLesson5JpaApplication.class, args);
+    }
+}
---
+++ 29_springAll/springboot-lesson5-jpa/src/main/java/com/jettjia/springboot/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.jettjia.springboot.controller;
+
+import com.jettjia.springboot.entity.User;
+import com.jettjia.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable("id") Integer id){
+        User user = userRepository.findOne(id);
+        return user;
+    }
+
+    @GetMapping("/user")
+    public User insertUser(User user){
+        User save = userRepository.save(user);
+        return save;
+    }
+}
