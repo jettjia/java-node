1a9abfb556c3daed038384bf135b3de3b4e47ba6
jidi <1608008898@qq.com>
2018-10-04 14:42:54 +0800

    add springboot jdbc,druid demo

---
+++ 29_springAll/springboot-lesson4-jdbc/src/main/java/com/jettjia/springboot/SpringbootLesson4JdbcApplication.java
@@ -0,0 +1,12 @@
+package com.jettjia.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLesson4JdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLesson4JdbcApplication.class, args);
+    }
+}
