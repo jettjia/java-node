486fefd781137ce17f933fcfc7b128318a24d1e4
jidi <1608008898@qq.com>
2018-10-10 14:25:48 +0800

    devtools

---
+++ 29_springAll/springboot-lesson12-deploy/src/main/java/com/jettjia/springboot/SpringbootLesson12DeployApplication.java
@@ -0,0 +1,12 @@
+package com.jettjia.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLesson12DeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLesson12DeployApplication.class, args);
+    }
+}
