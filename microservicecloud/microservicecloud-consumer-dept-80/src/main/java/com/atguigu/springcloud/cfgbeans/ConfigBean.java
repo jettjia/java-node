93d0271980a533301a6cfa6323a99d9f5c5e38db
jidi <1608008898@qq.com>
2018-10-16 10:15:09 +0800

    add ribbon

--- microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -17,14 +17,6 @@ public class ConfigBean //boot -->spring   applicationContext.xml --- @Configura
 	{
 		return new RestTemplate();
 	}
-	
-	@Bean
-	public IRule myRule()
-	{
-		//return new RoundRobinRule();
-		//return new RandomRule();//达到的目的，用我们重新选择的随机算法替代默认的轮询。
-		return new RetryRule();
-	}
 }
 
 //@Bean
