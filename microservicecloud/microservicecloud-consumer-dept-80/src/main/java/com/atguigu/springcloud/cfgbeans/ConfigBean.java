3f38600cc4bcff2d7925b505cbd6710c83557166
jidi <1608008898@qq.com>
2018-10-16 11:16:31 +0800

    add ribbon

--- microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import com.netflix.loadbalancer.RetryRule;
 
 @Configuration
@@ -16,6 +17,11 @@ public class ConfigBean //boot -->spring   applicationContext.xml --- @Configura
 	public RestTemplate getRestTemplate()
 	{
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public IRule myRule() {
+	    return new RandomRule(); //达到的目的，用我们重新选择的随机算法替代默认的轮询。
 	}
 }
 
